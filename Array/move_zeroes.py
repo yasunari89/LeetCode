5c67163d8c56e41212d8402dc9f3a6a672476c08
yasunari89 <yasunari89@gmail.com>
2020-05-12 21:40:06 +0900

    add Move Zeroes

---
+++ Array/move_zeroes.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)-1, -1, -1):
+            if not nums[i]:
+                nums.append(nums[i])
+                nums.pop(i)
