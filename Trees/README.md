3ba4eff4bc6fbb755a366a6dc8e4933c92ba9512
yasunari89 <yasunari89@gmail.com>
2020-05-19 04:49:40 +0900

    add convert sorted array to BST

--- Trees/README.md
+++ Trees/README.md
@@ -10,3 +10,4 @@
 | Validate Binary Search Tree | x | | やり方がいろいろあって網羅しようとすると結構むずい |
 | Symmetric Tree | x | | 幅優先探索なのはわかった<br />そこから対称性を検出するためには同じroot nodeを2つqueueに突っ込むんで比較するというのがわからなかった |
 | Binary Tree Level Order Traversal | o | |  |
+| Convert Sorted Array To Binary Search Tree | x | | 再帰やqueueで解ける（つまりDFS）<br />半分ずつ左右に振り分けるのを繰り返す |
---
+++ Trees/convert_sorted_array_to_binary_search_tree.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        if nums == []:
+            return None
+        if len(nums) == 1:
+            return TreeNode(nums[0])
+        n = len(nums)
+        mid = n // 2
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[:mid])
+        node.right = self.sortedArrayToBST(nums[mid+1:])
+        return node
+        
